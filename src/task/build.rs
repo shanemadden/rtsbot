59509d5e19dbc4e32f754673d34e1512ddc1a0f6
Shane Madden <shanemadden@users.noreply.github.com>
2023-06-28 23:20:10 -0600

    build

--- src/task/build.rs
+++ src/task/build.rs
@@ -1,12 +1,36 @@
-use screeps::{local::ObjectId, objects::ConstructionSite};
+use screeps::{local::ObjectId, objects::ConstructionSite, constants::ErrorCode};
 
-use crate::{task::TaskResult, worker::WorkerReference};
+use crate::{task::TaskResult, worker::WorkerReference, movement::{MovementGoal, MovementProfile}};
 
 pub fn build(worker: &WorkerReference, target: &ObjectId<ConstructionSite>) -> TaskResult {
     match worker {
         WorkerReference::Creep(creep) => {
             match target.resolve() {
-                Some(construction_site) => todo!(),
+                Some(construction_site) => {
+                    match creep.build(&construction_site) {
+                        Ok(()) => TaskResult::StillWorking(None),
+                        Err(e) => match e {
+                            ErrorCode::NotInRange => {
+                                let move_goal = MovementGoal {
+                                    goal: construction_site.pos().into(),
+                                    goal_range: 1,
+                                    priority: 1,
+                                    profile: MovementProfile::PlainsOneToOne,
+                                    avoid_creeps: false,
+                                };
+                                TaskResult::StillWorking(Some(move_goal))
+                            },
+                            ErrorCode::InvalidTarget => {
+                                // creep's standing on the construction site, and it's not walkable
+                                // should maybe make it flee..
+                                TaskResult::Complete
+                            },
+                            _ => {
+                                TaskResult::Complete
+                            }
+                        }
+                    }
+                },
                 // the construction site is either gone or not in a visible room;
                 // a good potential enhancement here is to include the position in the build task
                 // enum, and check for visibility (moving there if not visible) before removing
