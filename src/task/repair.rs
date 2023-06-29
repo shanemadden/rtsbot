def4f91df79eadd500ce4acbd212c839158e8d19
Shane Madden <shanemadden@users.noreply.github.com>
2023-06-28 23:32:12 -0600

    more love for build, plus repair and upgrade

--- src/task/repair.rs
+++ src/task/repair.rs
@@ -1,17 +1,36 @@
-use screeps::{local::ObjectId, objects::Structure};
+use log::*;
+use screeps::{constants::ErrorCode, local::ObjectId, objects::Structure};
 
-use crate::{task::TaskResult, worker::WorkerReference};
+use crate::{task::TaskResult, worker::WorkerReference, movement::{MovementGoal, MovementProfile}};
 
 pub fn repair(worker: &WorkerReference, target: &ObjectId<Structure>) -> TaskResult {
     match worker {
-        WorkerReference::Creep(creep) => {
-            match target.resolve() {
-                Some(target_structure) => todo!(),
-                // the repair target is either gone or not in a visible room;
-                // a good potential enhancement here is to include the position in the repair task
-                // enum, and check for visibility (moving there if not visible) before removing
-                None => TaskResult::Complete,
-            }
+        WorkerReference::Creep(creep) => match target.resolve() {
+            Some(target_structure) => {
+                match creep.repair(&target_structure) {
+                    Ok(()) => TaskResult::StillWorking(None),
+                    Err(e) => match e {
+                        ErrorCode::NotInRange => {
+                            let move_goal = MovementGoal {
+                                goal: target_structure.pos().into(),
+                                goal_range: 1,
+                                priority: 1,
+                                profile: MovementProfile::RoadsOneToTwo,
+                                avoid_creeps: false,
+                            };
+                            TaskResult::StillWorking(Some(move_goal))
+                        },
+                        e => {
+                            info!("repair failure: {:?}", e);
+                            TaskResult::Complete
+                        }
+                    }
+                }
+            },
+            // the repair target is either gone or not in a visible room;
+            // a good potential enhancement here is to include the position in the repair task
+            // enum, and check for visibility (moving there if not visible) before removing
+            None => TaskResult::Complete,
         }
         _ => panic!("unsupported worker type!"),
     }
--- src/task/upgrade.rs
+++ src/task/upgrade.rs
@@ -1,11 +1,32 @@
-use screeps::{local::ObjectId, objects::StructureController};
+use log::*;
+use screeps::{constants::ErrorCode, local::ObjectId, objects::StructureController};
 
-use crate::{task::TaskResult, worker::WorkerReference};
+use crate::{task::TaskResult, worker::WorkerReference, movement::{MovementGoal, MovementProfile}};
 
 pub fn upgrade(worker: &WorkerReference, target: &ObjectId<StructureController>) -> TaskResult {
     match worker {
         WorkerReference::Creep(creep) => match target.resolve() {
-            Some(controller) => todo!(),
+            Some(controller) => {
+                match creep.upgrade_controller(&controller) {
+                    Ok(()) => TaskResult::StillWorking(None),
+                    Err(e) => match e {
+                        ErrorCode::NotInRange => {
+                            let move_goal = MovementGoal {
+                                goal: controller.pos().into(),
+                                goal_range: 1,
+                                priority: 1,
+                                profile: MovementProfile::RoadsOneToTwo,
+                                avoid_creeps: false,
+                            };
+                            TaskResult::StillWorking(Some(move_goal))
+                        },
+                        e => {
+                            info!("upgrade failure: {:?}", e);
+                            TaskResult::Complete
+                        }
+                    }
+                }
+            },
             None => TaskResult::Complete,
         },
         _ => panic!("unsupported worker type!"),
