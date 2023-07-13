30b97329e42e08dc21ebad31c2d830f0063c9eb0
Shane Madden <shanemadden@users.noreply.github.com>
2023-07-12 23:41:26 -0600

    little formatting bits in main.js

--- javascript/main.js
+++ javascript/main.js
@@ -24,7 +24,7 @@ module.exports.loop = function () {
         // when a creep is spawned) won't trigger an attempt to parse RawMemory. Replace the object
         // with one unattached to memory magic - game functions will access the `Memory` object and
         // can throw data in here, and it'll go away at the end of tick.
-        
+
         // Because it's in place, RawMemory's string won't be thrown to JSON.parse to deserialize -
         // and because that didn't happen, RawMemory._parsed isn't set and won't trigger a
         // post-tick serialize.
@@ -43,12 +43,12 @@ module.exports.loop = function () {
             } else {
                 // attempt to load the wasm only if there's lots of bucket
                 if (Game.cpu.bucket < 1000) {
-                    console.log("low CPU, waiting" + JSON.stringify(Game.cpu));
+                    console.log("low bucket for wasm compile, waiting" + JSON.stringify(Game.cpu));
                     return;
                 }
-                // load the module, which we do here instead of at the top of the filebecause that
-                // can potentially cause the module to be unable to load if it's too heavy and trap
-                // the load cycle with no bucket to recover
+                // load the module, which we do here instead of at the top of the file, because 
+                // that can potentially cause the module to be unable to load if it's too heavy,
+                // and trap the load cycle with no bucket to recover
                 wasm_module = require(MODULE_NAME);
                 // setup wasm instance, which attaches at wasm_module.__wasm
                 wasm_module.initialize_instance();
